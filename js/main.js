058bc46435ea084b2a6cbaa5b07fd431f170384c
Vikec69 <D21526@oaopava.cz>
2024-11-16 16:41:04 +0100

    email funguje lesgoo

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message')
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        //serviceID - templateID - #form - publicKey
+        emailjs.sendForm('service_hxucsdk','template_hwkiuvq','#contact-form','FCvEIeoBh_AxGm3ur')
+
+        .then(() => {
+            //Zpráva se poslala
+            contactMessage.textContent = 'Zpráva byla úspěšně odeslána ✅'
+
+            //Odebrání úspěšně poslané zprávy po 5 sekundách
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000)
+
+            //Vymazání polí
+            contactForm.reset()
+        }, () => {
+
+            //Ukázaní Error zprávy
+            contactMessage.textContent = 'Zpráva neodeslána, (Chyba služby) ❌'
+        })
+    }
+    contactForm.addEventListener('submit', sendEmail)
