0eacf5d41c470e1a9827fd68481911e5455a859f
Vikec69 <D21526@oaopava.cz>
2025-03-10 15:35:58 +0100

    opravene videa v playlistu

--- playlist.php
+++ playlist.php
@@ -1,16 +1,28 @@
 <?php 
 require "./bts/common.php"; 
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-    $playlistData = $conn -> prepare("SELECT * FROM `playlisty` WHERE ID = ?");
-    $playlistData -> bind_param("i",$_GET['PlaylistID']);
-    $playlistData -> execute();
-    $queryResult = $playlistData -> get_result();
-    $result = $queryResult -> fetch_assoc();
-    $UserData = $conn -> prepare("SELECT * FROM `users` WHERE Username = ?");
-    $UserData -> bind_param("s",$result['CreatedBy']);
-    $UserData -> execute();
-    $queryresult = $UserData -> get_result();
-    $UserResult = $queryresult-> fetch_assoc();
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
+if (isset($_GET['PlaylistID'])) {
+    $playlistID = $_GET['PlaylistID'];
+
+    // Načtení playlistu
+    $playlistData = $conn->prepare("SELECT * FROM `playlisty` WHERE ID = ?");
+    $playlistData->bind_param("i", $playlistID);
+    $playlistData->execute();
+    $queryResult = $playlistData->get_result();
+    $result = $queryResult->fetch_assoc();
+
+    // Načtení údajů o uživateli, který playlist vytvořil
+    $UserData = $conn->prepare("SELECT users.Username, users.ProfilePic FROM `playlisty` JOIN users ON playlisty.CreatedBy = users.ID WHERE users.ID = ?");
+    $UserData->bind_param("i", $result['CreatedBy']);
+    $UserData->execute();
+    $queryresult = $UserData->get_result();
+    $UserResult = $queryresult->fetch_assoc();
+} else {
+    die("Chyba: Playlist ID není nastaveno.");
+}
 ?>
 <!DOCTYPE html>
 <html lang="cs">
@@ -54,7 +66,7 @@ require "./bts/common.php";
                    <img src="<?php echo $UserResult['ProfilePic']?>" alt="">
                      <div>
                         <h3><?php                     
-                        echo $result["CreatedBy"];
+                        echo $UserResult["CreatedBy"];
                     ?></h3>
                         <span><?php                     
                         echo $result["CreatedAt"];
@@ -86,34 +98,35 @@ require "./bts/common.php";
                  ?>
                </div>
             </div>
-            <!-- Obsah playlistu -->
+            <!-- Výpis videí v playlistu -->
     </section> 
          <section class = "projects section">
             <h2 class="section__title">Videa v playlistu</h2>
             <div class="projects__container container grid">
                 <?php
-                foreach($splitington as $video_id){
-                    $VideoData = $conn -> prepare("SELECT * FROM `videa` WHERE ID = ?");
-                    $VideoData -> bind_param("i", $video_id);
-                    $VideoData -> execute();
-                    $queryResult = $VideoData -> get_result();
-                    $result = $queryResult -> fetch_assoc();
+                // Načtení všech videí patřících do daného playlistu
+                        $VideoData = $conn->prepare("SELECT * FROM videa WHERE Playlist = ?");
+                        $VideoData->bind_param("i", $playlistID);
+                        $VideoData->execute();
+                        $queryResult = $VideoData->get_result();
+
+                while ($video = $queryResult->fetch_assoc()) {
                 echo'
                 <article class="projects__card">
-                    <a href="video.php?VideoID='. $video_id .'" class="projects__image"><img src="'. $result["Thumbnail"] .'" alt="image" class="projects__img">
+                    <a href="video.php?VideoID='. $video["ID"] .'" class="projects__image"><img src="'. $video["Thumbnail"] .'" alt="image" class="projects__img">
                     </a>
         
                     <div class="projects__data">
-                        <h3 class="projects__name">'. $result["VidName"] .'</h3>
-                        <p class="projects__description">'. $result["Description"].'.</p>
+                        <h3 class="projects__name">'. $video["VidName"] .'</h3>
+                        <p class="projects__description">'. $video["Description"].'.</p>
                     </div>
 
-                    <a href="video.php?VideoID='. $video_id .'" class="projects__button">
+                    <a href="video.php?VideoID='. $video["ID"] .'" class="projects__button">
                         <i class="ri-links-line"></i>
                         <span>Přehrát video</span>
                     </a>
                 </article>';
-                }
+            }
                 ?>
             </div>
         </section>
